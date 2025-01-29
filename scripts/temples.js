ba2d9ff307d581fcaf8b444c8b9b4e45a08b0622
Bruno Teixeira <166347569+TXzzera@users.noreply.github.com>
2025-01-28 23:03:51 -0300

    new att

---
+++ scripts/temples.js
@@ -0,0 +1,14 @@
+const currentYear = new Date().getFullYear();
+document.getElementById('currentYear').textContent = `© ${currentYear} Bruno de Sousa Teixeira - São Paulo, Brazil`;
+
+const lastModified = document.lastModified;
+document.getElementById('lastModified').textContent = `Last updated: ${lastModified}`;
+
+document.addEventListener("DOMContentLoaded", function () {
+    const menuButton = document.getElementById("menu-toggle");
+    const menu = document.getElementById("menu");
+
+    menuButton.addEventListener("click", function () {
+        menu.classList.toggle("show"); // Alterna a visibilidade do menu
+    });
+});
